adea0380232f8061221eeae1f7d8b71ab5c45899
Andreas Vestergaard <apvestergaard@gmail.com>
2015-08-27 15:02:01 +0200

    before disconnect outlets

--- Surfcast/ViewController.swift
+++ Surfcast/ViewController.swift
@@ -9,10 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var userCity: UITextField!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+    }
 
+    @IBOutlet var resultLabel: UILabel!
+    
+    
+    func ShowError() {
+        
+        resultLabel.text = "Not able to find weather for " + userCity.text + ". Please try again"
+
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest")
+        
+        if url != nil {
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+           
+            var urlError = false
+            
+            if error == nil {
+                
+                var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
+                println(urlContent)
+                
+                
+            } else {
+                
+                urlError = true
+                
+            }
+            
+            if urlError == true {
+                
+                self.ShowError()
+                
+            }
+            
+        })
+            
+            task.resume()
+    
+        } else {
+            
+            ShowError()
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
