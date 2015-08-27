4d1694a4a9c59e5b39479668d0299a0926034090
Andreas Vestergaard <apvestergaard@gmail.com>
2015-08-27 16:06:25 +0200

    updated outlets and data retrieving

--- Surfcast/ViewController.swift
+++ Surfcast/ViewController.swift
@@ -10,18 +10,82 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
     @IBOutlet var userCity: UITextField!
     
     @IBAction func findWeather(sender: AnyObject) {
+        
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/" + userCity.text.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if url != nil {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                
+                var urlError = false
+                
+                var weather = ""
+                
+                if error == nil {
+                    
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding) as NSString!
+                    
+                    var urlContentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+                    
+                    if urlContentArray.count > 1 {
+                        
+                        var weatherArray = urlContentArray[1].componentsSeparatedByString("</span>")
+                        
+                        weather = weatherArray[0] as! String
+                        
+                        weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                    } else {
+                        
+                        urlError = true
+                        
+                    }
+                    
+                    
+                    
+                } else {
+                    
+                    urlError = true
+                    
+                }
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    if urlError == true {
+                        
+                        self.showError()
+                        
+                    } else {
+                        
+                        self.resultLabel.text = weather
+                        
+                    }
+                }
+                
+            })
+            
+            task.resume()
+            
+            
+        } else {
+            
+            showError()
+            
+        }
+        
+        
     }
-
+    
     @IBOutlet var resultLabel: UILabel!
     
-    
-    func ShowError() {
+    func showError() {
         
-        resultLabel.text = "Not able to find weather for " + userCity.text + ". Please try again"
-
+        resultLabel.text = "Was not able to find weather for " + userCity.text + ". Please try again"
+        
     }
     
     
@@ -29,50 +93,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var url = NSURL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest")
-        
-        if url != nil {
-        
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
-           
-            var urlError = false
-            
-            if error == nil {
-                
-                var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
-                
-                println(urlContent)
-                
-                
-            } else {
-                
-                urlError = true
-                
-            }
-            
-            if urlError == true {
-                
-                self.ShowError()
-                
-            }
-            
-        })
-            
-            task.resume()
-    
-        } else {
-            
-            ShowError()
-            
-        }
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
