4ca620f025dcf8baea8068e645ce85d2a68a270a
Andreas Vestergaard <apvestergaard@gmail.com>
2015-08-27 12:32:19 +0200

    Initial Commit

---
+++ Surfcast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Surfcast
+//
+//  Created by Andreas Vestergaard on 27/08/15.
+//  Copyright (c) 2015 Andreas Vestergaard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
